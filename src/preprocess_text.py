09edb7c00d52031125783f083c568be66dd254a2
Vinit Gela <vinitgela@gmail.com>
2021-08-31 16:36:14 +0530

    added the seed source codes for bootcamp

---
+++ src/preprocess_text.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 31 16:29:33 2021
+
+@author: vg11995
+"""
+import pandas
+
+import string
+
+
+def remove_punctuation(text):
+    """
+    text: input text to remove punctuations from
+    returns cleaned version of the text
+    """
+    
+    punctuationfree="".join([i for i in text if i not in string.punctuation])
+    return punctuationfree
+
+
+    
+    
